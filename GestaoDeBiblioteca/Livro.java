5d503d5b9615b56143e3dce420f7d49ed32b9132
PC-Istar <pauloc.alquino@gmail.com>
2025-09-25 20:38:44 -0300

    Update Livro.java

--- GestaoDeBiblioteca/Livro.java
+++ GestaoDeBiblioteca/Livro.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package GestaoDeBiblioteca;
 
-/**
- *
- * @author roney
- */
 public class Livro {
 
     private String titulo;
@@ -35,3 +27,4 @@ public class Livro {
     }
     
 }
+
