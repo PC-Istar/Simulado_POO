fd0a255783587d5c0c7d9cb773c661d305717b8a
PC-Istar <pauloc.alquino@gmail.com>
2025-09-25 20:44:13 -0300

    Update Main_Escola.java

--- GestaoEscolar/Main_Escola.java
+++ GestaoEscolar/Main_Escola.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package GestaoEscolar;
 
 public class Main_Escola {
@@ -20,3 +16,4 @@ public class Main_Escola {
     }
  
 }
+
