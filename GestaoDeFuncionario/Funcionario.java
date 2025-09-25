01c5cf0433355c346e33815a29bbddb206ce7bc2
PC-Istar <pauloc.alquino@gmail.com>
2025-09-25 20:40:29 -0300

    Update Funcionario.java

--- GestaoDeFuncionario/Funcionario.java
+++ GestaoDeFuncionario/Funcionario.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package GestaoDeFuncionario;
 
 public class Funcionario {
@@ -21,4 +17,5 @@ public class Funcionario {
         return salario;
     }
 }
+
 
