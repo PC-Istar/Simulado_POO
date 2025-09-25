4dd1cd2577ed0af5dabfc90c9f18d5f0306c589e
PC-Istar <pauloc.alquino@gmail.com>
2025-09-25 20:43:16 -0300

    Update Moto.java

--- GestaoDeVeiculos/Moto.java
+++ GestaoDeVeiculos/Moto.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package GestaoDeVeiculos;
 
-/**
- *
- * @author roney
- */
 public class Moto extends Veiculo{
     private int cilindradas;
 
@@ -27,3 +19,4 @@ public class Moto extends Veiculo{
     
     
 }
+
