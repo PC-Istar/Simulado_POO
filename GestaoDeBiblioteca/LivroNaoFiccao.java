0b739bae53c7fb9059464ab3f5143e87fba63511
PC-Istar <pauloc.alquino@gmail.com>
2025-09-25 20:39:27 -0300

    Update LivroNaoFiccao.java

--- GestaoDeBiblioteca/LivroNaoFiccao.java
+++ GestaoDeBiblioteca/LivroNaoFiccao.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package GestaoDeBiblioteca;
 
-/**
- *
- * @author roney
- */
 public class LivroNaoFiccao extends Livro{
     private String assunto;
     
@@ -24,3 +16,4 @@ public class LivroNaoFiccao extends Livro{
         this.assunto = assunto;
     }
 }
+
