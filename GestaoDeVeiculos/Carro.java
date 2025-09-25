b757804fdf995bbf6b19474e36795b52d51ac525
PC-Istar <pauloc.alquino@gmail.com>
2025-09-25 20:42:43 -0300

    Update Carro.java

--- GestaoDeVeiculos/Carro.java
+++ GestaoDeVeiculos/Carro.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package GestaoDeVeiculos;
 
-/**
- *
- * @author roney
- */
 public class Carro extends Veiculo {
     private int numeroDePortas;
     
@@ -24,3 +16,4 @@ public class Carro extends Veiculo {
     }
     
 }
+
