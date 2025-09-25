5e8d28cd7d71780a04bcf2c09db6ae9749f3454f
PC-Istar <pauloc.alquino@gmail.com>
2025-09-25 19:47:59 -0300

    Add files via upload

---
+++ SupermercadoOnline/Main_Produto.java
@@ -0,0 +1,12 @@
+package SupermercadoOnline;
+public class Main_Produto {
+    public static void main(String[] args) {
+       Produto p1 = new Produto();
+       
+       p1.nome = "Roupas";
+       p1.preco = 100.00;
+       p1.exibirInfo();
+       p1.aplicarDesconto(2);
+    }
+    
+}
